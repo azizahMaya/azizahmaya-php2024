e563dc90b1eee81a4fc8a2ab4162d3ca05d48369
azizahMaya <sitinurulazizah222@gmail.com>
2024-11-11 11:30:47 +0700

    g

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
+<DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale-1">
+        <title>Login Siswa - Aplikasi Pembayaran SPP</title>
+        <link href="css/boostraf.min.css"rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-md-center">
+            <div class="col-md-4">
+                <h4 class="text-center">LOGIN SISWA</h4>
+                <div class="card">
+                    <div class="card-header">
+                        <img src="logo spp.jpg" width="100%">
+                    </div>
+                    <div class="card-body">
+                        <from acation="proses-login-siswa.php" methode="post">
+                            <div class="from-group mb-2">
+                                <tabel>NISN</table>
+                                <imput type="number" name="nisn" class="from-control"placeholder="Masukan
+                                NIS anda.." required>
+                        </div>
+                        <div class="card-body">
+                        <from acation="proses-login-siswa.php" methode="post">
+                            <div class="from-group mb-2">
+                                <tabel>NIS</table>
+                                <imput type="number" name="nis" class="from-control"placeholder="Masukan
+                                NIS anda.." required>
+                        </div>
+                </from>
+</div>
+</div>
+</div>
+</div>
 
+</div>
+
+<script src="js/boostrap.bundle.min.js"></script>
+
+</body>
+</html>
