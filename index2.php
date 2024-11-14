e2a253a49ecbb8a038e44b411601447433e5fbca
azizahMaya <sitinurulazizah222@gmail.com>
2024-11-14 16:52:43 +0700

    Add files via upload
    AzizahMaya

--- index2.php
+++ index2.php
@@ -6,12 +6,11 @@
     <title>Login - Admin / Petugas Aplikasi Pembayaran SPP.</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-<<<<<<< HEAD
 <body>  
 <div class="container mt-5">
     <div class="row justify-content-md-center">
         <div class="col-md-4">
-            <center><h4 class="text-center">LOGIN ADMIN/PETUGAS </h4></center>
+            <center><h4 class="text-center">LOGIN SISWA</h4></center>
             <div class="card">
                 <div class="card-header">
                     <!-- logo --> 
@@ -22,36 +21,6 @@
                         <div class="form-group mb-2">
                             <label>Username</label>
                             <input type="text" name="username" class="form-control" placeholder="Masukan Username anda.." require>                         
-=======
-<body>
-    <div class="container mt-5">
-        <div class="row justify-content-md-center">
-            <div class="col-md-4">
-                <center><h4>LOGIN SISWA</h4></center>
-                <div class="card">
-                    <div class="card-header">
-                            <!--logo-->
-                            <center><img src="img/logo.jpg" width="20%"></center>
-                    </div>
-                    <div class="card-body">
-                        <from acation="proses-login-siswa.php" methode="post">
-                            <div class="from-group mb-2">
-                            <center><label>NISN</label>
-                                <input type="number" name="nisn" class="from-control"placeholder="Masukan NISN anda.." required>
-                                </center>
-                            </div>
-                        <div class="card-body">
-                        <from acation="proses-login-siswa.php" methode="post">
-                            <div class="from-group mb-2">
-                            <center><label>NIS</label>
-                                <input type="number" name="nis" class="from-control"placeholder="Masukan NIS anda.." required>
-                                </center>
-                            </div>
-                        <div class="from group mb-2">
-                                <center><button type="submit" class"btn btn-primary"> LOGIN </button></center>
-                            </div>
-                          <center><a href="index2.php">Login sebagai administrator/petugas </a></center>
->>>>>>> 8050d8f875bc084608c1c4d9e48fff168fd89cde
                             
                         </div>
                         <div class="form-group mb-2">
